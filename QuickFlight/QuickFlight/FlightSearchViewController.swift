60240fdb8c96bf39914232c2922091bcd0572cdd
jessicawiradinata <jessicawiradinata@gmail.com>
2019-05-22 10:15:48 +1000

    Added mock rows to flight search results screen

--- QuickFlight/QuickFlight/FlightSearchViewController.swift
+++ QuickFlight/QuickFlight/FlightSearchViewController.swift
@@ -8,17 +8,32 @@
 
 import UIKit
 
-class FlightSearchViewController: UIViewController {
-
+class FlightSearchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var flightResult: UITableView!
+    @IBOutlet weak var flightResultsTable: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        flightResultsTable.dataSource = self
+        flightResultsTable.delegate = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "flightResultCell", for: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 85
     }
     
 
