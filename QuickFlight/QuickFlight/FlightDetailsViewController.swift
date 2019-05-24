a6189021295b73fd2602a8b8e96524ddb38a9616
Kevin Kevin <12419141@student.uts.edu.au>
2019-05-24 15:11:07 +1000

    Checklist button and delete have been created

--- QuickFlight/QuickFlight/FlightDetailsViewController.swift
+++ QuickFlight/QuickFlight/FlightDetailsViewController.swift
@@ -8,15 +8,15 @@
 
 import UIKit
 
-class FlightDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FlightDetailsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, ChangeButton, CreateItem {
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var items: [Item] = [Item(itemName: "Banana")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        items.append(Item(itemName: "apple"))
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -26,9 +26,43 @@ class FlightDetailsViewController: UIViewController, UITableViewDelegate, UITabl
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "itemCell", for: indexPath) as! ItemCell
         
-        cell.itemLabel.text = items[indexPath.row].itemName
+        cell.itemTextfield.text = items[indexPath.row].itemName
+        
+        if items[indexPath.row].checked {
+            cell.itemCheckbox.setBackgroundImage(UIImage(named: "checkboxFilled"), for: .normal)
+        } else {
+            cell.itemCheckbox.setBackgroundImage(UIImage(named: "checkboxOutline"), for: .normal)
+        }
+        
+        if (indexPath.row == 0) {
+            if (items[indexPath.row].itemName != "") {
+                items.append(Item(itemName: ""))
+            }
+        }
+        
+        cell.changeButtonDelegate = self
+        cell.createItemDelegate = self
+        cell.indexP = indexPath.row
+        cell.items = items
         
         return cell
+    }
+    
+    func changeButton(checked: Bool, index: Int) {
+        items[index].checked = checked
+        tableView.reloadData()
+    }
+    
+    func createItem(itemName: String, index: Int) {
+        items[index].itemName = itemName
+        tableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            self.items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
     }
 }
 
