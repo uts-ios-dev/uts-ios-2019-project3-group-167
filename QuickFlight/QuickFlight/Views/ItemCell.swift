766cd76274938f326dd29caf6bdd0e0673fab766
jessicawiradinata <jessicawiradinata@gmail.com>
2019-06-04 20:10:51 +1000

    Directory restructure

--- QuickFlight/QuickFlight/Views/ItemCell.swift
+++ QuickFlight/QuickFlight/Views/ItemCell.swift
@@ -12,14 +12,17 @@ protocol ChangeChecklistButton {
     func changeChecklistButton(done: Bool, index: Int)
 }
 
+/// Custom cell for checklist display
 class ItemCell: UITableViewCell, UITextFieldDelegate {
+    
     @IBOutlet weak var itemCheckbox: UIButton!
     @IBOutlet weak var itemTextfield: UITextField!
     
     var checklistButtonDelegate: ChangeChecklistButton?
-    var indexP: Int?
     var checklists: [Checklist]?
+    var indexP: Int?
     
+    /// Toggles checklist icon to checked and not checked
     @IBAction func checkboxAction(_ sender: UIButton) {
         if checklists![indexP!].done {
             checklistButtonDelegate?.changeChecklistButton(done: false, index: indexP!)
@@ -28,12 +31,8 @@ class ItemCell: UITableViewCell, UITextFieldDelegate {
         }
     }
     
+    /// Saves item name
     @IBAction func itemTextfieldAction(_ sender: UITextField) {
-        // This section will save the data to a new array when database is connected.
         checklists![indexP!].name = itemTextfield.text!
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        itemTextfield.resignFirstResponder()
     }
 }
